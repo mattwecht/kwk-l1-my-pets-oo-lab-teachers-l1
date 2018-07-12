3bd6c0b4f7a2d268e8b8dbb5b929fe4be2224a8b
mattwecht <mattwecht@gmail.com>
2018-07-12 12:48:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
-  # code goes here
+  attr_reader :name, :mood
+  attr_writer :mood
+  def initialize(name="sam", mood = "nervous")
+    @name = name
+    @mood = mood
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,29 @@
 class Owner
-  # code goes here
+  attr_reader :species, :pets, :name
+  attr_writer :name
+  def initialize (species = "human", name = "katie")
+    @species = species 
+    @name = name
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def say_species
+    return "I am a human."
+  end
+  
+  def buy_fish(name)
+      fish = Fish.new(name)
+      @pets[:fishes] = [fish]
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] = [cat]
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dog] = [dog]
+  end
 
  end
